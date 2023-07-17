1ff3379b5d39f71550525cbd86b953f28ba08c7f
Kace <lakacey03@gmail.com>
2023-07-17 11:25:55 -0400

    camera multithreading

--- data_retrieval/run_visualization_server.py
+++ data_retrieval/run_visualization_server.py
@@ -4,11 +4,19 @@ import csv
 import io
 import os
 from copy import deepcopy
-
+import time
 import cv2
 import numpy as np
 from PyQt6 import QtNetwork, QtWidgets, uic
+
+from PyQt6.QtGui import *
+from PyQt6.QtWidgets import *
+from PyQt6.QtCore import *
+import traceback, sys
+
 from pyqtgraph.Qt import QtCore
+
+cap = cv2.VideoCapture(0)
 
 from csi_extraction import (
     calc_phase_angle,
@@ -17,23 +25,75 @@ from csi_extraction import (
     unpack_csi_struct,
 )
 
+def create_plot(field, color):
+    return field.plot(pen={"color": color, "width": 3})
+
+class WorkerSignals(QObject):
+    finished = pyqtSignal()
+    error = pyqtSignal(tuple)
+    result = pyqtSignal(object)
+
+class Worker(QRunnable):
+
+    def __init__(self, fn, *args, **kwargs):
+        super(Worker, self).__init__()
+        # Store constructor arguments (re-used for processing)
+        self.fn = fn
+        self.args = args
+        self.kwargs = kwargs
+        self.signals = WorkerSignals()
+
+    @pyqtSlot()
+    def run(self):
+        try:
+            result = self.fn(*self.args, **self.kwargs)
+        except:
+            traceback.print_exc()
+            exctype, value = sys.exc_info()[:2]
+            self.signals.error.emit((exctype, value, traceback.format_exc()))
+        else:
+            self.signals.result.emit(result)
+        finally:
+            self.signals.finished.emit()
+
 
 class UI(QtWidgets.QWidget):
+    
+    def print_output(self, s):
+        print(s)
+
+    def print_finished(self):
+        print("Finished recording!")
+
     def __init__(
         self,
         app: QtWidgets.QApplication,
         parent: QtWidgets.QWidget = None,
         is_5ghz: bool = True,
+        make_video: bool = False,
+        max_frames: int = 500
     ):
         super(UI, self).__init__(parent)
+        self.make_video = make_video
+
+        self.write_to_csv = True
+        if (self.make_video):
+            self.threadpool = QThreadPool()
+            print("Multithreading with maximum %d threads" % self.threadpool.maxThreadCount())
+            worker = Worker(self.useCamera)
+            worker.signals.result.connect(self.print_output)
+            worker.signals.finished.connect(self.thread_complete)
+
+            self.threadpool.start(worker)
+
         self.app = app
+        self.count = 0
+        self.max_frames = max_frames
         self.is_5ghz = is_5ghz
 
         # get and show object and layout
         uic.loadUi("window.ui", self)
         self.setWindowTitle(f"Visualize CSI data {'5GHz' if is_5ghz else '2.4GHz'}")
-
-        self.antenna_pairs, self.carrier, self.amplitude, self.phase = [], [], [], []
 
         amp = self.box_amp
         amp.setBackground("w")
@@ -43,9 +103,6 @@ class UI(QtWidgets.QWidget):
         amp.setYRange(0, 1, padding=0)  # for normalized amp values, prev range: 0, 90
         # amp.setYRange(0, 400, padding=0)  # for normalized amp values, prev range: 0, 90
         amp.setXRange(0, 114 if is_5ghz else 57, padding=0)
-
-        def create_plot(field, color):
-            return field.plot(pen={"color": color, "width": 3})
         
         self.colors = np.array([
             (200, 0, 0),
@@ -60,20 +117,6 @@ class UI(QtWidgets.QWidget):
         ])
 
         self.penAmps = np.array(list(map(lambda x: create_plot(amp, x), self.colors))).reshape(3, 3)
-        # self.penAmps[0].set
-        # self.penAmp0_0 = create_plot(amp, (200, 200, 0))
-        # print(self.penAmp0_0)
-        # print("EHEHEHEHEEEaaaaaaaaaaaaaaaa")
-
-        # self.penAmp0_1 = amp.plot(pen={"color": (200, 200, 0), "width": 3})
-        # self.penAmp0_2 = amp.plot(pen={"color": (137, 49, 239), "width": 3})
-        # self.penAmp1_0 = amp.plot(pen={"color": (0, 0, 200), "width": 3})
-        # self.penAmp1_1 = amp.plot(pen={"color": (0, 200, 200), "width": 3})
-        # self.penAmp1_2 = amp.plot(pen={"color": (255, 0, 189), "width": 3})
-        # self.penAmp2_0 = amp.plot(pen={"color": (135, 233, 17), "width": 3})
-        # self.penAmp2_1 = amp.plot(pen={"color": (255, 127, 80), "width": 3})
-        # self.penAmp2_2 = amp.plot(pen={"color": (0, 0, 0), "width": 3})
-        # self.ampList = []
 
         phase = self.box_phase
         phase.setBackground("w")
@@ -83,86 +126,54 @@ class UI(QtWidgets.QWidget):
         phase.setYRange(-np.pi - 0.2, np.pi + 0.2, padding=0)
         phase.setXRange(0, 114 if is_5ghz else 57, padding=0)
         self.penPhases = np.array(list(map(lambda x: create_plot(phase, x), self.colors))).reshape(3, 3)
-
-        self.penPhase0_1 = phase.plot(pen={"color": (200, 200, 0), "width": 3})
-        self.penPhase0_2 = phase.plot(pen={"color": (137, 49, 239), "width": 3})
-        self.penPhase1_0 = phase.plot(pen={"color": (0, 0, 200), "width": 3})
-        self.penPhase1_1 = phase.plot(pen={"color": (0, 200, 200), "width": 3})
-        self.penPhase1_2 = phase.plot(pen={"color": (255, 0, 189), "width": 3})
-        self.penPhase2_0 = phase.plot(pen={"color": (135, 233, 17), "width": 3})
-        self.penPhase2_1 = phase.plot(pen={"color": (255, 127, 80), "width": 3})
-        self.penPhase2_2 = phase.plot(pen={"color": (0, 0, 0), "width": 3})
-
         self.amp = amp
         self.box_phase = phase
 
     @QtCore.pyqtSlot()
     def update_plots(self):
-        # print(self.amplitude)
-        # print(str(np.shape(self.amplitude)) + "HEREE")
-        # print(np.shape(self.amplitude[:, 0, 0]))
-        # print("HEREEE")
+        self.count = self.count + 1
+        print(self.count)
+        if self.antenna_pairs:
+            for i in range(self.amplitude.shape[1]):
+                for j in range(self.amplitude.shape[2]):
+                    self.penAmps[i, j].setData(self.carrier, self.amplitude[:, i, j])
+                    self.penPhases[i, j].setData(self.carrier, self.phase[:, i, j])
 
-        # print(self.amplitude[:][0][0])
-
-        # print(self.amplitude[0:55][0][0], )
-        # print(self.amplitude.())
-        if len(self.amplitude) and len(self.amplitude[0]):
-            if len(self.antenna_pairs) > 0:
-                # self.penAmps[0].setData(self.carrier, self.amplitude[:, 0, 0])
-                # print(np.shape(self.amplitude)[0])
-                for i in range(self.amplitude.shape[1]):
-                    for j in range(self.amplitude.shape[2]):
-                        self.penAmps[i, j].setData(self.carrier, self.amplitude[:, i, j])
-                        self.penPhases[i, j].setData(self.carrier, self.phase[:, i, j])
-                # self.penAmps[0].setData(self.carrier, self.phase[0])
-
-                # if len(self.antenna_pairs) > 1:
-                #     # self.penAmp0_1.setData(self.carrier, self.amplitude[1])
-                #     self.penPhase0_1.setData(self.carrier, self.phase[1])
-
-                # if len(self.antenna_pairs) > 2:
-                #     # self.penAmp1_0.setData(self.carrier, self.amplitude[2])
-                #     self.penPhase1_0.setData(self.carrier, self.phase[2])
-
-                # if len(self.antenna_pairs) > 3:
-                #     pass
-                #     # self.penAmp1_1.setData(self.carrier, self.amplitude[3])
-                #     self.penPhase1_1.setData(self.carrier, self.phase[3])
-
-                # if len(self.antenna_pairs) > 4:
-                #     # self.penAmp0_2.setData(self.carrier, self.amplitude[4])
-                #     self.penPhase0_2.setData(self.carrier, self.phase[4])
-
-                # if len(self.antenna_pairs) > 5:
-                #     pass
-                #     # self.penAmp1_2.setData(self.carrier, self.amplitude[5])
-                #     self.penPhase1_2.setData(self.carrier, self.phase[5])
-
-                # if len(self.antenna_pairs) > 6:
-                #     pass
-                #     # self.penAmp2_2.setData(self.carrier, self.amplitude[6])
-                #     self.penPhase2_2.setData(self.carrier, self.phase[6])
-
-                # if len(self.antenna_pairs) > 7:
-                #     pass
-                #     # self.penAmp2_1.setData(self.carrier, self.amplitude[7])
-                #     self.penPhase2_1.setData(self.carrier, self.phase[7])
-
-                # if len(self.antenna_pairs) > 8:
-                #     pass
-                #     # self.penAmp2_2.setData(self.carrier, self.amplitude[8])
-                #     self.penPhase2_2.setData(self.carrier, self.phase[8])
-
-        self.process_events()  # force complete redraw for every plot
+        self.process_events()
 
     def process_events(self):
         self.app.processEvents()
+    
+    def useCamera(self):
+
+        width = int(cap.get(cv2.CAP_PROP_FRAME_WIDTH))
+        height = int(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
+
+
+        # TODO remove the hardcode filename
+        filename = 'video.mp4'
+
+        writer = cv2.VideoWriter(filename, cv2.VideoWriter_fourcc(*'DIVX'), 20, (width, height))
+        total_frames = 0
+        while total_frames < self.max_frames:
+            ret, frame= cap.read()
+
+            writer.write(frame)
+
+            if cv2.waitKey(1) & 0xFF == 27:
+                break
+            print("Frame: " + str(total_frames))
+            total_frames = total_frames + 1
+
+        cap.release()
+        writer.release()
+        cv2.destroyAllWindows()
+
+        # Stop writing to the CSV
+        self.write_to_csv = False
 
 
 class UDPListener:
-    packet_counter = 0
-
     def __init__(
         self,
         save_data_path: str,
@@ -174,7 +185,7 @@ class UDPListener:
         self.save_data_path = save_data_path
         os.makedirs(save_data_path, exist_ok=True)
         self.filename = filename
-
+        self.packet_counter = 0
         self.make_photo = make_photo
         self.cam = cv2.VideoCapture(0) if self.make_photo else None
 
@@ -191,83 +202,59 @@ class UDPListener:
             )
             f = io.BytesIO(datagram)
             csi_inf = unpack_csi_struct(f)
-
             if (
                 csi_inf.csi != 0
             ):  # get csi from data packet, save and process for further visualization
                 (
-                    raw_peak_amplitudes,
+                    raw_amplitudes,
                     raw_phases,
                     carriers_indexes,
                     antenna_pairs,
-                    nam,
-                    nph
                 ) = self.get_csi_raw_data(csi_inf)
 
-                if (len(raw_peak_amplitudes)):
-                    self.calc(
-                        nam, nph, carriers_indexes, antenna_pairs
-                    )
-                    self.save_csi_to_file(raw_peak_amplitudes, raw_phases, carriers_indexes)
+                self.set_plot_data(
+                    raw_amplitudes, raw_phases, carriers_indexes, antenna_pairs
+                )
 
+                # Only save 3x3 tensors
+                if (len(antenna_pairs) == 3 * 3 and self.form.write_to_csv):
+                    self.save_csi_to_file(csi_inf.num_tones, csi_inf.timestamp, raw_amplitudes, raw_phases)
                     if self.make_photo:
                         self.make_photo_and_save()
 
-                    self.form.update_plots()
-                    self.packet_counter += 1
+                # TODO, this plot more easily breaks at higher package sent 
+                self.form.update_plots()
+                self.packet_counter += 1
 
-    def get_csi_raw_data(self, csi_inf) -> np.ndarray | None:
-        num_of_antenna_pairs = csi_inf.nc * csi_inf.nr
-
-        print("getting csi from the raw data")
-
-
-        channel = csi_inf.channel
+    def get_csi_raw_data(self, csi_inf) -> np.ndarray:
+        print("Processing CSI Info")
         carriers_num = csi_inf.num_tones
         carriers = np.arange(csi_inf.num_tones)
 
-        new_raw_phases = np.zeros([carriers_num, 3, 3])
-        new_raw_amps = np.zeros([carriers_num, 3, 3])
-
-        if (num_of_antenna_pairs < 9):
-            print("WRONG NUMBER OF ANTENNAS...")
-            # return [], None, None, None
-            # # exit(1)
-        print("channel: ", channel)
-        print("carriers_num: ", carriers_num)
+        raw_amps = np.zeros([carriers_num, 3, 3])
+        raw_phases = np.zeros([carriers_num, 3, 3])
 
         antenna_pairs = [
             (tx_index, rx_index)
             for tx_index in range(csi_inf.nc)
             for rx_index in range(csi_inf.nr)
         ]
-        raw_phases, raw_peak_amplitudes = [[] for _ in range(num_of_antenna_pairs)], [
-            [] for _ in range(num_of_antenna_pairs)
-        ]
-        print("antenna_pairs: ", antenna_pairs)
-
         for i in range(carriers_num):
-            for enum_index, (tr_i, rc_i) in enumerate(antenna_pairs):
-                # print("csi_len ", csi_inf.csi_len)
-                # print("csi_inf.csi[i]: ", csi_inf)
+            for _, (tr_i, rc_i) in enumerate(antenna_pairs):
                 p = csi_inf.csi[i][tr_i][rc_i]
                 imag, real = p.imag, p.real
 
                 peak_amplitude = np.sqrt(
                     np.power(real, 2) + np.power(imag, 2)
-                )  # calculate peak amplitude
-                phase_angle = calc_phase_angle(p)  # calculate phase angle
+                )
+                phase_angle = calc_phase_angle(p)
 
-                raw_peak_amplitudes[enum_index].append(peak_amplitude)
-                raw_phases[enum_index].append(phase_angle)
+                raw_amps[i][tr_i][rc_i] = peak_amplitude
+                raw_phases[i][tr_i][rc_i] = phase_angle
 
-                new_raw_phases[i][tr_i][rc_i] = phase_angle
-                new_raw_amps[i][tr_i][rc_i] = peak_amplitude
-        # print(new_raw_amps)
+        return raw_amps, raw_phases, carriers, antenna_pairs
 
-        return raw_peak_amplitudes, raw_phases, carriers, antenna_pairs, new_raw_amps, new_raw_phases
-
-    def calc(
+    def set_plot_data(
         self, raw_peak_amplitudes, raw_phases, carriers_indexes, antenna_pairs
     ):  
         # Used for visualization only
@@ -281,29 +268,28 @@ class UDPListener:
 
         self.form.amplitude = amplitude
 
-        # print(phase.shape)
         # Calibrate phase and update form phase values
         for i in range(phase.shape[1]):
             for j in range(phase.shape[2]):
                 phase[:, i, j] = np.array(calibrate_phase(phase[:, i, j].flatten())).reshape(56)
-        # phase = calibrate_phase(phase)
-        # for i in range(len(phase)):
-        #     print(len(phase[i]))
-        #     phase[i] = calibrate_phase(phase[i])
-        # phase = phase.reshape(3, 2)
+
         self.form.phase = phase
         self.form.antenna_pairs = antenna_pairs
 
     def make_photo_and_save(self):
+        start = time.time()
         ret, frame = self.cam.read()
+        
 
         img_name = "opencv_frame_{}.png".format(self.packet_counter)
         path_to_image = "{}/{}/{}".format(self.save_data_path, "images", img_name)
 
         cv2.imwrite(path_to_image, frame)
+        end = time.time()
         print("{} written!".format(path_to_image))
+        print("Execution time to take picture:", (end-start) * 10**3, "ms")
 
-    def save_csi_to_file(self, raw_peak_amplitudes, raw_phases, carriers):
+    def save_csi_to_file(self, num_carriers, timestamp, raw_amplitudes, raw_phases):
         # print("Saving CSI data to .csv file...")
         filename_csv = self.filename
         path_to_csv_file = "{}/{}".format(self.save_data_path, filename_csv)
@@ -314,23 +300,14 @@ class UDPListener:
             )
             writer.writerow(
                 [
-                    *carriers,
-                    *raw_peak_amplitudes[0],
-                    *raw_peak_amplitudes[1],
-                    *raw_peak_amplitudes[2],
-                    *raw_peak_amplitudes[3],
-                    *raw_phases[0],
-                    *raw_phases[1],
-                    *raw_phases[2],
-                    *raw_phases[3],
+                    num_carriers, timestamp, *raw_amplitudes.flatten(), *raw_phases.flatten()
                 ]
             )
-
 
 def init_argparse() -> argparse.ArgumentParser:
     parser = argparse.ArgumentParser(
         description="Visualization server that listens to any incoming packets, plot them and store.\n"
-        "Only supports up to 4 antenna pairs at the moment.",
+        "Supports up to 9 antenna pairs at the moment.",
         prog="python run_visualization_server.py",
     )
 
@@ -344,21 +321,29 @@ def init_argparse() -> argparse.ArgumentParser:
     parser.add_argument(
         "-s",
         "--save_path",
-        help="path to the folder where to save the incoming data",
+        help="Path to the folder where to save the incoming data",
         default="./tmp",
         type=str,
     )
     parser.add_argument(
-        "-p", "--port", help="port to listen to", default=60000, type=int
+        "-p", "--port", help="Port to listen to", default=60000, type=int
     )
     parser.add_argument(
         "--photo",
-        help="make webcam photo for each data packet?",
+        help="Make webcam photo for each data packet?",
         default=False,
         type=bool,
     )
+
     parser.add_argument(
-        "--file", help="name of the file to save to?", default="data.csv", type=str
+        "--video",
+        help="Make a video until vp packets",
+        default=False,
+        type=bool,
+    )
+
+    parser.add_argument(
+        "--file", help="Name of the file to save to?", default="data.csv", type=str
     )
 
     return parser
@@ -371,16 +356,18 @@ def run_app() -> None:
     try:
         udp_socket = QtNetwork.QUdpSocket()
         udp_socket.bind(QtNetwork.QHostAddress.SpecialAddress.Any, args.port)
+        is_5ghz = args.frequency == "5000MHZ"
+        make_video = args.video
 
-        form = UI(app=app, is_5ghz=(args.frequency == "5000MHZ"))
+        form = UI(app = app, is_5ghz = is_5ghz, make_video=make_video)
         form.show()
 
         listener = UDPListener(
             save_data_path=args.save_path,
-            sock=udp_socket,
-            form=form,
-            filename=args.file,
-            make_photo=args.photo,
+            sock = udp_socket,
+            form = form,
+            filename = args.file,
+            make_photo = args.photo,
         )
         app.exec()
 
@@ -389,12 +376,10 @@ def run_app() -> None:
             listener.cam.release()
         except Exception as e_cam:
             pass
-
         print("KeyboardInterrupt. Finishing the program...")
 
 
 if __name__ == "__main__":
-    # x = np.reshape(np.arange(56 * 3 * 3), [56, 3, 3])
-    # print(np.shape(x))
-    # print(x[:, 0, 0])
+    print("Sleeping for 5 seconds... Get ready!")
+    time.sleep(5)
     run_app()
